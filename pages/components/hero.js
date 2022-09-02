4b66b303228b484143cc54220f82946081a90162
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2022-09-01 22:51:30 -0400

    Fix comments error

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -3,11 +3,11 @@ export default function Hero() {
 		<div className="hero">
 			<h1>
 				<div>
-					// <strong>FULL-STACK DEVELOPER</strong>
+					{'//'} <strong>FULL-STACK DEVELOPER</strong>
 				</div>
-				<div>// SYSTEMS ADMINISTRATOR </div>
+				<div>{'//'} SYSTEMS ADMINISTRATOR </div>
 				<div>
-					// <strong>IN NEW YORK CITY</strong>
+					{'//'} <strong>IN NEW YORK CITY</strong>
 				</div>
 			</h1>
 		</div>
