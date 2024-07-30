0258b84b2484bf6d75473496cb52720fce2e4f61
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2024-07-29 20:55:02 -0400

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,22 @@
+export default function sitemap() {
+	return [
+		{
+			url: 'https://ismaeelakram.com',
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 1,
+		},
+		{
+			url: 'https://ismaeelakram.com/experience',
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 1,
+		},
+		{
+			url: 'https://ismaeelakram.com/projects',
+			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 1,
+		},
+	];
+}
